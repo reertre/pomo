0993e609cdfad05e46e75a0d165ab7848218218e
Devraj Changkakati <devraj.changkakati@gmail.com>
2023-06-06 12:09:04 +0530

    Update Pomodoro.js

--- Pomodoro.js
+++ Pomodoro.js
@@ -55,6 +55,20 @@ const Pomodoro = () => {
     return () => clearInterval(intervalId);
   }, [timerRunning, timeRemaining, isBreak]);
 
+  const renderTime = ({ remainingTime }) => {
+    if (remainingTime === 0) {
+      return <div className="timer">Too lale...</div>;
+    }
+
+    return (
+      <div className="timer">
+        <div className="text">Remaining</div>
+        <div className="value">{remainingTime}</div>
+        <div className="text">seconds</div>
+      </div>
+    );
+  };
+
   return (
     <div className="pomodoro">
       <div className="timer">
