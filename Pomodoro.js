b189d1f229194d95df3201d217d17d097855a5c7
Devraj Changkakati <devraj.changkakati@gmail.com>
2023-06-07 01:05:14 +0530

    Update Pomodoro.js

--- Pomodoro.js
+++ Pomodoro.js
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from "react";
-import styles from "./pomodoro.module.css";
+import React, { useState } from "react";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
+import styles from "./pomodoro.module.css";
 
 const Pomodoro = () => {
-  const [timeRemaining, setTimeRemaining] = useState(25 * 60);
   const [timerRunning, setTimerRunning] = useState(false);
-  const [isBreak, setIsBreak] = useState(false);
 
   const startTimer = () => {
     setTimerRunning(true);
@@ -15,90 +13,47 @@ const Pomodoro = () => {
     setTimerRunning(false);
   };
 
-  const resetTimer = () => {
-    setTimeRemaining(25 * 60);
-    setTimerRunning(false);
-    setIsBreak(false);
-  };
-
-  const formatTime = (time) => {
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
-
-    return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
-  };
-
-  const calculateProgress = () => {
-    const progress = ((25 * 60 - timeRemaining) / (25 * 60)) * 100;
-    return progress > 100 ? 100 : progress;
-  };
-
-  useEffect(() => {
-    let intervalId;
-
-    if (timerRunning && timeRemaining > 0) {
-      intervalId = setInterval(() => {
-        setTimeRemaining((prevTime) => prevTime - 1);
-      }, 1000);
-    } else if (timerRunning && timeRemaining === 0) {
-      clearInterval(intervalId);
-
-      if (!isBreak) {
-        setTimeRemaining(5 * 60);
-        setIsBreak(true);
-      } else {
-        setTimeRemaining(25 * 60);
-        setIsBreak(false);
-      }
-    }
-
-    return () => clearInterval(intervalId);
-  }, [timerRunning, timeRemaining, isBreak]);
-
   const renderTime = ({ remainingTime }) => {
     if (remainingTime === 0) {
       return <div className={styles.timer}>Too late...</div>;
     }
 
     return (
-      <div className={styles.timerText}>
-        <span>{formatTime(timeRemaining)}</span>
-        {isBreak ? <p>Break</p> : <p>Focus</p>}
+      <div className={styles.timer}>
+        <div className={styles.timerCircle}>
+          <CountdownCircleTimer
+            isPlaying={timerRunning}
+            duration={25 * 60}
+            colors={[["#004777", 0.33], ["#F7B801", 0.33], ["#A30000"]]}
+            strokeWidth={12}
+            size={240}
+            trailColor="#f4f4f4"
+            onComplete={() => {
+              setTimerRunning(false);
+            }}
+          >
+            {({ remainingTime }) => (
+              <div className={styles.timerText}>
+                <span>{remainingTime}</span>
+                <p>Focus</p>
+              </div>
+            )}
+          </CountdownCircleTimer>
+        </div>
+        <div className={styles.timerControls}>
+          <button onClick={startTimer}>Start</button>
+          <button onClick={stopTimer}>Stop</button>
+        </div>
       </div>
     );
   };
 
   return (
     <div className={styles.pomodoroPage}>
-      <section className={styles.wrapperBg}>
-        <div className={styles.container}>
-          <section className={styles.donutSection}>
-            <h1 className={styles.pomodoroTitle}>Pomodoro</h1>
-            <div className={styles.timerWrapper}>
-              <CountdownCircleTimer
-                isPlaying={timerRunning}
-                duration={timeRemaining}
-                colors={[["#004777", 0.33], ["#F7B801", 0.33], ["#A30000"]]}
-                onComplete={() => {
-                  setTimerRunning(false);
-                  setTimeRemaining(25 * 60);
-                  setIsBreak(!isBreak);
-                }}
-              >
-                {renderTime}
-              </CountdownCircleTimer>
-            </div>
-          </section>
-
-          <section className={styles.buttonSection}>
-            <div className={styles.timerControls}>
-              <button onClick={startTimer}>Start</button>
-              <button onClick={stopTimer}>Stop</button>
-              <button onClick={resetTimer}>Reset</button>
-            </div>
-          </section>
-        </div>
-      </section>
+      <div className={styles.pomodoro}>
+        <h1>Pomodoro</h1>
+        <div className={styles.timerWrapper}>{renderTime}</div>
+      </div>
     </div>
   );
 };
