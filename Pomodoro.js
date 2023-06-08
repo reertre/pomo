8f1799aaede32b3b52b65a29798077f525f945b6
Devraj Changkakati <devraj.changkakati@gmail.com>
2023-06-08 09:33:42 +0530

    Update Pomodoro.js

--- Pomodoro.js
+++ Pomodoro.js
@@ -1,147 +1,55 @@
-import React, { useState, useEffect } from "react";
-import { CircularProgressbar } from "react-circular-progressbar";
-import "react-circular-progressbar/dist/styles.css";
-import { slide as Menu } from "react-burger-menu";
-import { Section, Button, ButtonGroup } from "@barclays/blueprint-react";
-import styles from "./pomodoro.module.css";
-import Link from "next/link";
+import React, { useState } from "react";
+import Sidebar from "react-sidebar";
+import "./Navbar.css";
 
-const Pomodoro = () => {
-  const [timeRemaining, setTimeRemaining] = useState(25 * 60);
-  const [timerRunning, setTimerRunning] = useState(false);
-  const [isBreak, setIsBreak] = useState(false);
+const App = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(true);
 
-  const startTimer = () => {
-    setTimerRunning(true);
-  };
-
-  const stopTimer = () => {
-    setTimerRunning(false);
-  };
-
-  const resetTimer = () => {
-    setTimeRemaining(25 * 60);
-    setTimerRunning(false);
-    setIsBreak(false);
-  };
-
-  const formatTime = (time) => {
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
-
-    return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
-  };
-
-  const calculateProgress = () => {
-    const progress = ((25 * 60 - timeRemaining) / (25 * 60)) * 100;
-    return progress > 100 ? 100 : progress;
-  };
-
-  useEffect(() => {
-    let intervalId;
-
-    if (timerRunning && timeRemaining > 0) {
-      intervalId = setInterval(() => {
-        setTimeRemaining((prevTime) => prevTime - 1);
-      }, 1000);
-    } else if (timerRunning && timeRemaining === 0) {
-      clearInterval(intervalId);
-
-      if (!isBreak) {
-        setTimeRemaining(5 * 60);
-        setIsBreak(true);
-      } else {
-        setTimeRemaining(25 * 60);
-        setIsBreak(false);
-      }
-    }
-
-    return () => clearInterval(intervalId);
-  }, [timerRunning, timeRemaining, isBreak]);
-
-  const renderTime = ({ remainingTime }) => {
-    if (remainingTime === 0) {
-      return <div className={styles.timer}>Too late...</div>;
-    }
-
-    return (
-      <div className={styles.timerText}>
-        <span>{formatTime(timeRemaining)}</span>
-        {isBreak ? <p>Break</p> : <p>Focus</p>}
-      </div>
-    );
+  const onSetSidebarOpen = (open) => {
+    setSidebarOpen(open);
   };
 
   return (
-    <div className={styles.pomodoroPage}>
-      <Menu
-        width={"200px"}
-        disableAutoFocus
-        customBurgerIcon={false}
-        styles={{
-          bmBurgerButton: {
-            display: "none"
-          },
-          bmMenu: {
-            background: "white",
-            padding: "1rem",
-            boxShadow: "0 0 10px rgba(0, 0, 0, 0.2)",
-            overflow: "hidden",
-            width: "200px",
-            marginTop: "20px",
-          },
-          bmItemList: {
-            color: "black",
-            padding: "0",
-            margin: "0",
-          },
-          bmItem: {
-            display: "block",
-            marginTop: "10px",
-          },
-          bmOverlay: {
-            background: "rgba(0, 0, 0, 0.3)",
-          },
-        }}
-      >
-        <Link href="/">
-          <a className={styles.menuItem}>Timer</a>
-        </Link>
-        <Link href="/">
-          <a className={styles.menuItem}>Stats</a>
-        </Link>
-        <Link href="/">
-          <a className={styles.menuItem}>Settings</a>
-        </Link>
-      </Menu>
-
-      <Section>
-        <section>
-          <h1>Pomodoro</h1>
-          <div className={styles.timerWrapper}>
-            <CircularProgressbar
-              value={calculateProgress()}
-              text={formatTime(timeRemaining)}
-              strokeWidth={10}
-            />
-          </div>
-          {renderTime({ remainingTime: timeRemaining })}
-        </section>
-      </Section>
-
-      <Section>
-        <section>
-          <div className={styles.timerControls}>
-            <ButtonGroup>
-              <Button onClick={startTimer}>Start</Button>
-              <Button onClick={stopTimer}>Stop</Button>
-              <Button onClick={resetTimer}>Reset</Button>
-            </ButtonGroup>
-          </div>
-        </section>
-      </Section>
-    </div>
+    <Sidebar
+      sidebar={
+        <div className="nav-menu">
+          <ul className="nav-menu-items">
+            
+            <li className="nav-text">
+              <button className="btn btn-primary">Timer</button>
+            </li>
+            <li className="nav-text">
+              <button className="btn btn-secondary">Settings</button>
+            </li>
+            <li className="nav-text">
+              <button className="btn btn-success">Statistics</button>
+            </li>
+          </ul>
+        </div>
+      }
+      open={sidebarOpen}
+      onSetOpen={onSetSidebarOpen}
+      docked={true}
+      styles={{
+        sidebar: {
+          background: "#fffff",
+          width: "250px",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          position: "fixed",
+          top: 0,
+          left: sidebarOpen ? "0" : "-100%",
+          transition: "350ms",
+        },
+        content: {
+          marginLeft: "250px",
+        },
+      }}
+    >
+      
+    </Sidebar>
   );
 };
 
-export default Pomodoro;
+export default App;
