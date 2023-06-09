7e1afaabaeab31c5403d443e667d2993b50e6e47
Devraj Changkakati <devraj.changkakati@gmail.com>
2023-06-09 23:24:17 +0530

    Update Pomodoro.js

--- Pomodoro.js
+++ Pomodoro.js
@@ -1,71 +1,29 @@
-import React, { useState } from 'react';
-import './Settings.css';
+import React from 'react';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+import styles from './SliderComponent.module.css'; // Import the CSS module
 
-const Settings = () => {
-  const [workDuration, setWorkDuration] = useState(5);
-  const [shortBreakDuration, setShortBreakDuration] = useState(1);
-  const [longBreakDuration, setLongBreakDuration] = useState(1);
-  const [rounds, setRounds] = useState(2);
-
-  const handleWorkDurationChange = (event) => {
-    setWorkDuration(parseInt(event.target.value));
-  };
-
-  const handleShortBreakDurationChange = (event) => {
-    setShortBreakDuration(parseInt(event.target.value));
-  };
-
-  const handleLongBreakDurationChange = (event) => {
-    setLongBreakDuration(parseInt(event.target.value));
-  };
-
-  const handleRoundsChange = (event) => {
-    setRounds(parseInt(event.target.value));
-  };
-
+const SliderComponent = () => {
   return (
-    <div className="centered-sliders">
-      <h2>Work Duration: {workDuration} mins</h2>
-      <input
-        type="range"
-        min="5"
-        max="60"
-        value={workDuration}
-        onChange={handleWorkDurationChange}
-        className="slider"
-      />
-
-      <h2>Short Break Duration: {shortBreakDuration} mins</h2>
-      <input
-        type="range"
-        min="1"
-        max="30"
-        value={shortBreakDuration}
-        onChange={handleShortBreakDurationChange}
-        className="slider"
-      />
-
-      <h2>Long Break Duration: {longBreakDuration} mins</h2>
-      <input
-        type="range"
-        min="1"
-        max="45"
-        value={longBreakDuration}
-        onChange={handleLongBreakDurationChange}
-        className="slider"
-      />
-
-      <h2>Rounds: {rounds}</h2>
-      <input
-        type="range"
-        min="2"
-        max="15"
-        value={rounds}
-        onChange={handleRoundsChange}
-        className="slider"
-      />
+    <div className={styles.sliderContainer}>
+      <div className={styles.slider}>
+        <div className={styles.sliderTitle}>Work Duration</div>
+        <Slider min={5} max={60} />
+      </div>
+      <div className={styles.slider}>
+        <div className={styles.sliderTitle}>Short Break Duration</div>
+        <Slider min={1} max={30} />
+      </div>
+      <div className={styles.slider}>
+        <div className={styles.sliderTitle}>Long Break Duration</div>
+        <Slider min={1} max={45} />
+      </div>
+      <div className={styles.slider}>
+        <div className={styles.sliderTitle}>Rounds</div>
+        <Slider min={2} max={15} />
+      </div>
     </div>
   );
 };
 
-export default Settings;
+export default SliderComponent;
