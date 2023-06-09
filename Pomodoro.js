1e62d50f14e37722a9fbc661e973382f05cc480d
Devraj Changkakati <devraj.changkakati@gmail.com>
2023-06-09 16:32:54 +0530

    Update Pomodoro.js

--- Pomodoro.js
+++ Pomodoro.js
@@ -1,21 +1,20 @@
-import React, { useState, useEffect } from "react";
-import { CircularProgressbar } from "react-circular-progressbar";
-import "react-circular-progressbar/dist/styles.css";
-import { Section, Button, ButtonGroup } from '@barclays/blueprint-react';
-import styles from "./pomodoro.module.css";
-import Link from "next/Link";
+import React, { useState } from "react";
 import Sidebar from "react-sidebar";
 import "./Navbar.css";
 
-const SidebarContent = () => {
+const SidebarContent = ({ setPage }) => {
   return (
     <div className={styles.navMenu}>
       <ul className={styles.navMenuItems}>
         <li className={styles.navText}>
-          <button className={`${styles.btn} ${styles.btnPrimary}`}>Timer</button>
+          <button className={`${styles.btn} ${styles.btnPrimary}`} onClick={() => setPage("timer")}>
+            Timer
+          </button>
         </li>
         <li className={styles.navText}>
-          <button className={`${styles.btn} ${styles.btnSecondary}`}>Settings</button>
+          <button className={`${styles.btn} ${styles.btnSecondary}`} onClick={() => setPage("settings")}>
+            Settings
+          </button>
         </li>
         <li className={styles.navText}>
           <button className={`${styles.btn} ${styles.btnSuccess}`}>Statistics</button>
@@ -25,77 +24,44 @@ const SidebarContent = () => {
   );
 };
 
-const Pomodoro = () => {
-  const [timeRemaining, setTimeRemaining] = useState(25 * 60);
-  const [timerRunning, setTimerRunning] = useState(false);
-  const [isBreak, setIsBreak] = useState(false);
+const TimerPage = () => {
+  return (
+    <div>
+      <h1>Pomodoro</h1>
+      {/* Timer and circular progress bar content */}
+    </div>
+  );
+};
+
+const SettingsPage = () => {
+  return (
+    <div>
+      <h1>Settings</h1>
+      {/* Only the settings content */}
+    </div>
+  );
+};
+
+const App = () => {
   const [sidebarOpen, setSidebarOpen] = useState(true);
+  const [activePage, setActivePage] = useState("timer");
 
   const onSetSidebarOpen = (open) => {
     setSidebarOpen(open);
   };
 
-  const startTimer = () => {
-    setTimerRunning(true);
-  };
-
-  const stopTimer = () => {
-    setTimerRunning(false);
-  };
-
-  const resetTimer = () => {
-    setTimeRemaining(25 * 60);
-    setTimerRunning(false);
-    setIsBreak(false);
-  };
-
-  const formatTime = (time) => {
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
-
-    return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
-  };
-
-  const calculateProgress = () => {
-    const progress = ((25 * 60 - timeRemaining) / (25 * 60)) * 100;
-    return progress > 100 ? 100 : progress;
-  };
-
-  useEffect(() => {
-    let intervalId;
-
-    if (timerRunning && timeRemaining > 0) {
-      intervalId = setInterval(() => {
-        setTimeRemaining((prevTime) => prevTime - 1);
-      }, 1000);
-    } else if (timerRunning && timeRemaining === 0) {
-      clearInterval(intervalId);
-
-      if (!isBreak) {
-        setTimeRemaining(5 * 60);
-        setIsBreak(true);
-      } else {
-        setTimeRemaining(25 * 60);
-        setIsBreak(false);
-      }
+  const renderPage = () => {
+    if (activePage === "timer") {
+      return <TimerPage />;
+    } else if (activePage === "settings") {
+      return <SettingsPage />;
     }
-
-    return () => clearInterval(intervalId);
-  }, [timerRunning, timeRemaining, isBreak]);
-
-  const renderTime = ({ remainingTime }) => {
-    if (remainingTime === 0) {
-      return <div className={styles.timer}>Too late...</div>;
-    }
-
-    return (
-      <div className={styles.timerText}></div>
-    );
+    // Add more conditions for other pages
   };
 
   return (
     <Sidebar
-      sidebar={<SidebarContent />}
+      sidebar={<SidebarContent setPage={setActivePage} />}
       open={sidebarOpen}
       onSetOpen={onSetSidebarOpen}
       docked={true}
@@ -117,37 +83,45 @@ const Pomodoro = () => {
       }}
     >
       <div className={styles.pomodoroPage}>
-        <Section>
-          <section>
+        {activePage === "timer" && (
+          <Section>
             <section>
-              <div>
-                <h1>Pomodoro</h1>
-                <div className={styles.timerWrapper}>
-                  <CircularProgressbar
-                    value={calculateProgress()}
-                    text={formatTime(timeRemaining)}
-                    strokeWidth={10}
-                  />
+              <section>
+                <div>
+                  <h1>Pomodoro</h1>
+                  <div className={styles.timerWrapper}>
+                    <CircularProgressbar
+                      value={calculateProgress()}
+                      text={formatTime(timeRemaining)}
+                      strokeWidth={10}
+                    />
 
-                  {renderTime({ remainingTime: timeRemaining })}
+                    {renderTime({ remainingTime: timeRemaining })}
+                  </div>
                 </div>
+              </section>
+            </section>
+
+            <section>
+              <div className={styles.timerControls}>
+                <ButtonGroup>
+                  <Button onClick={startTimer}>Start</Button>
+                  <Button onClick={stopTimer}>Stop</Button>
+                  <Button onClick={resetTimer}>Reset</Button>
+                </ButtonGroup>
               </div>
             </section>
-          </section>
+          </Section>
+        )}
 
-          <section>
-            <div className={styles.timerControls}>
-              <ButtonGroup>
-                <Button onClick={startTimer}>Start</Button>
-                <Button onClick={stopTimer}>Stop</Button>
-                <Button onClick={resetTimer}>Reset</Button>
-              </ButtonGroup>
-            </div>
-          </section>
-        </Section>
+        {activePage === "settings" && (
+          <Section>
+            <SettingsPage />
+          </Section>
+        )}
       </div>
     </Sidebar>
   );
 };
 
-export default Pomodoro;
+export default App;
