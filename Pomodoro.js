f7fde1d6e1d0a4dcdf91e93a7b0732a36af8b77d
Devraj Changkakati <devraj.changkakati@gmail.com>
2023-06-14 16:47:42 +0530

    Update Pomodoro.js
    Oy

--- Pomodoro.js
+++ Pomodoro.js
@@ -1,21 +1,166 @@
-useEffect(() => {
-  let intervalId;
+import React, { useState, useEffect } from "react";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import { slide as Menu } from "react-burger-menu";
+import { Section, Button, ButtonGroup } from "@barclays/blueprint-react";
+import styles from "./pomodoro.module.css";
+import Link from "next/link";
+import Card from "./Card"; // Import your Card component here
 
-  if (timerRunning && timeRemaining > 0) {
-    intervalId = setInterval(() => {
-      setTimeRemaining((prevTime) => prevTime - 1);
-    }, 1000);
-  } else if (timerRunning && timeRemaining === 0) {
-    if (!isBreak) {
-      setShowModal(true);
-      setIsBreak(true);
-    } else {
-      setTimeRemaining(25 * 60);
-      setIsBreak(false);
-    }
-  }
+const Pomodoro = () => {
+  const [timeRemaining, setTimeRemaining] = useState(25 * 60);
+  const [timerRunning, setTimerRunning] = useState(false);
+  const [isBreak, setIsBreak] = useState(false);
 
-  return () => {
-    clearInterval(intervalId);
+  const startTimer = () => {
+    setTimerRunning(true);
   };
-}, [timerRunning, timeRemaining, isBreak]);
+
+  const stopTimer = () => {
+    setTimerRunning(false);
+  };
+
+  const resetTimer = () => {
+    setTimeRemaining(25 * 60);
+    setTimerRunning(false);
+    setIsBreak(false);
+  };
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+
+    return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+  };
+
+  const calculateProgress = () => {
+    const progress = ((25 * 60 - timeRemaining) / (25 * 60)) * 100;
+    return progress > 100 ? 100 : progress;
+  };
+
+  useEffect(() => {
+    let intervalId;
+
+    if (timerRunning && timeRemaining > 0) {
+      intervalId = setInterval(() => {
+        setTimeRemaining((prevTime) => prevTime - 1);
+      }, 1000);
+    } else if (timerRunning && timeRemaining === 0) {
+      clearInterval(intervalId);
+
+      if (!isBreak) {
+        setTimeRemaining(5 * 60);
+        setIsBreak(true);
+      } else {
+        setTimeRemaining(25 * 60);
+        setIsBreak(false);
+      }
+    }
+
+    return () => clearInterval(intervalId);
+  }, [timerRunning, timeRemaining, isBreak]);
+
+  const renderTime = ({ remainingTime }) => {
+    if (remainingTime === 0) {
+      return <div className={styles.timer}>Too late...</div>;
+    }
+
+    return (
+      <div className={styles.timerText}>
+        <span>{formatTime(timeRemaining)}</span>
+        {isBreak ? <p>Break</p> : <p>Focus</p>}
+      </div>
+    );
+  };
+
+  const renderCards = () => {
+    if (!isBreak && timeRemaining === 0) {
+      return (
+        <Section>
+          <div className={styles.cardsContainer}>
+            <Card title="Card 1" />
+            <Card title="Card 2" />
+            <Card title="Card 3" />
+          </div>
+        </Section>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <div className={styles.pomodoroPage}>
+      <Menu
+        width={"200px"}
+        disableAutoFocus
+        customBurgerIcon={false}
+        styles={{
+          bmBurgerButton: {
+            display: "none",
+          },
+          bmMenu: {
+            background: "white",
+            padding: "1rem",
+            boxShadow: "0 0 10px rgba(0, 0, 0, 0.2)",
+            overflow: "hidden",
+            width: "200px",
+            marginTop: "20px",
+          },
+          bmItemList: {
+            color: "black",
+            padding: "0",
+            margin: "0",
+          },
+          bmItem: {
+            display: "block",
+            marginTop: "10px",
+          },
+          bmOverlay: {
+            background: "rgba(0, 0, 0, 0.3)",
+          },
+        }}
+      >
+        <Link href="/">
+          <a className={styles.menuItem}>Timer</a>
+        </Link>
+        <Link href="/">
+          <a className={styles.menuItem}>Stats</a>
+        </Link>
+        <Link href="/">
+          <a className={styles.menuItem}>Settings</a>
+        </Link>
+      </Menu>
+
+      <Section>
+        <section>
+          <h1>Pomodoro</h1>
+          <div className={styles.timerWrapper}>
+            <CircularProgressbar
+              value={calculateProgress()}
+              text={formatTime(timeRemaining)}
+              strokeWidth={10}
+            />
+          </div>
+          {renderTime({ remainingTime: timeRemaining })}
+        </section>
+      </Section>
+
+      <Section>
+        <section>
+          <div className={styles.timerControls}>
+            <ButtonGroup>
+              <Button onClick={startTimer}>Start</Button>
+              <Button onClick={stopTimer}>Stop</Button>
+              <Button onClick={resetTimer}>Reset</Button>
+            </ButtonGroup>
+          </div>
+        </section>
+      </Section>
+
+      {renderCards()}
+    </div>
+  );
+};
+
+export default Pomodoro;
+      
