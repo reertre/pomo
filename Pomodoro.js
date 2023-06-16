f1dc97502176a8c8b02f5640e0630ecb4bceca01
Devraj Changkakati <devraj.changkakati@gmail.com>
2023-06-16 15:36:48 +0530

    Update Pomodoro.js

--- Pomodoro.js
+++ Pomodoro.js
@@ -1,13 +1,69 @@
 import React, { useState, useEffect } from "react";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import { Section, Button, ButtonGroup, Typography, Pagination } from '@barclays/blueprint-react';
 import styles from "./pomodoro.module.css";
-import { CountdownCircleTimer } from "react-countdown-circle-timer";
+import Sidebar from "react-sidebar";
+import "./Navbar.css";
+
+const SidebarContent = () => {
+  return (
+    <div className={styles.navMenu}>
+      <ul className={styles.navMenuItems}>
+        <li className={styles.navText}>
+          <button className={`${styles.btn} ${styles.btnPrimary}`}>Timer</button>
+        </li>
+        <li className={styles.navText}>
+          <button className={`${styles.btn} ${styles.btnSecondary}`}>Settings</button>
+        </li>
+        <li className={styles.navText}>
+          <button className={`${styles.btn} ${styles.btnSuccess}`}>Statistics</button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+const Slideshow = () => {
+  const [activeCard, setActiveCard] = useState(0);
+  const cards = [
+    { title: "Focus", content: "Focus on your task" },
+    { title: "Short Break", content: "Take a short break" },
+    { title: "Long Break", content: "Take a long break" },
+  ];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveCard((prevActiveCard) => (prevActiveCard + 1) % cards.length);
+    }, 3000); // Change card every 3 seconds
+
+    return () => clearInterval(interval);
+  }, [cards.length]);
+
+  return (
+    <div>
+      {cards.map((card, index) => (
+        <div key={index} style={{ display: index === activeCard ? "block" : "none" }}>
+          <h1>{card.title}</h1>
+          <section>
+            <p>{card.content}</p>
+          </section>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 const Pomodoro = () => {
-  const [focusTimeRemaining, setFocusTimeRemaining] = useState(25 * 60);
-  const [breakTimeRemaining, setBreakTimeRemaining] = useState(5 * 60);
+  const [timeRemaining, setTimeRemaining] = useState(45);
   const [timerRunning, setTimerRunning] = useState(false);
   const [isBreak, setIsBreak] = useState(false);
-  const [showCards, setShowCards] = useState(false); // New state variable to track if cards are shown
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+  const [page, setPage] = useState(1);
+
+  const onSetSidebarOpen = (open) => {
+    setSidebarOpen(open);
+  };
 
   const startTimer = () => {
     setTimerRunning(true);
@@ -18,8 +74,7 @@ const Pomodoro = () => {
   };
 
   const resetTimer = () => {
-    setFocusTimeRemaining(25 * 60);
-    setBreakTimeRemaining(5 * 60);
+    setTimeRemaining(45);
     setTimerRunning(false);
     setIsBreak(false);
   };
@@ -31,106 +86,88 @@ const Pomodoro = () => {
     return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
   };
 
-  const calculateProgress = (timeRemaining) => {
-    const totalTime = isBreak ? 5 * 60 : 25 * 60;
-    const progress = ((totalTime - timeRemaining) / totalTime) * 100;
+  const calculateProgress = () => {
+    const progress = ((45 - timeRemaining) / 45) * 100;
     return progress > 100 ? 100 : progress;
+  };
+
+  const handleChange = (event, value) => {
+    setPage(value);
   };
 
   useEffect(() => {
     let intervalId;
 
-    if (timerRunning && focusTimeRemaining > 0) {
+    if (timerRunning && timeRemaining > 0) {
       intervalId = setInterval(() => {
-        setFocusTimeRemaining((prevTime) => prevTime - 1);
+        setTimeRemaining((prevTime) => prevTime - 1);
       }, 1000);
-    } else if (timerRunning && focusTimeRemaining === 0) {
+    } else if (timerRunning && timeRemaining === 0) {
       clearInterval(intervalId);
 
       if (!isBreak) {
-        setBreakTimeRemaining(5 * 60);
+        setTimeRemaining(300); // Set 5 minutes for short break
         setIsBreak(true);
+        setPage(2); // Update page to short break
       } else {
-        setFocusTimeRemaining(25 * 60);
+        setTimeRemaining(900); // Set 15 minutes for long break
         setIsBreak(false);
+        setPage(3); // Update page to long break
       }
     }
 
     return () => clearInterval(intervalId);
-  }, [timerRunning, focusTimeRemaining, isBreak]);
-
-  const renderTime = ({ remainingTime }) => {
-    if (remainingTime === 0) {
-      return <div className={styles.timer}>Too late...</div>;
-    }
-
-    return (
-      <div className={styles.timerText}>
-        <span>{formatTime(isBreak ? breakTimeRemaining : focusTimeRemaining)}</span>
-        {isBreak ? <p>Break</p> : <p>Pomodoro</p>}
-      </div>
-    );
-  };
-
-  const renderCardsSection = () => {
-    if (!timerRunning && focusTimeRemaining === 0 && isBreak && showCards) {
-      return (
-        <Section>
-          <div className={styles.cardsSection}>
-            {/* Add your cards component or JSX here */}
-            <h2>Cards Section</h2>
-            {/* Example card */}
-            <div className={styles.card}>Card 1</div>
-            <div className={styles.card}>Card 2</div>
-            <div className={styles.card}>Card 3</div>
-          </div>
-        </Section>
-      );
-    }
-    return null;
-  };
-
-  useEffect(() => {
-    if (showCards) {
-      // Set the timer duration to 45 seconds when the cards are shown
-      setFocusTimeRemaining(45);
-      setIsBreak(false);
-    }
-  }, [showCards]);
+  }, [timerRunning, timeRemaining, isBreak]);
 
   return (
-    <div className={styles.pomodoroPage}>
-      <section>
+    <Sidebar
+      sidebar={<SidebarContent />}
+      open={sidebarOpen}
+      onSetOpen={onSetSidebarOpen}
+      docked={true}
+      styles={{
+        sidebar: {
+          background: "#fffff",
+          width: "250px",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          position: "fixed",
+          top: 0,
+          left: sidebarOpen ? "0" : "-100%",
+          transition: "350ms",
+        },
+        content: {
+          marginLeft: "250px",
+        },
+      }}
+    >
+      <div className={styles.pomodoroPage}>
         <Section>
-          <section>
-            <div className={styles.Pomodoro}>
-              <h1 styles={{ marginTop: "20px", padding: "20px" }}>Pomodoro</h1>
-              <div className={styles.timerwrapper}>
-                <CountdownCircleTimer
-                  isPlaying={timerRunning}
-                  duration={isBreak ? breakTimeRemaining : focusTimeRemaining}
-                  colors={[["#004777", 0.33], ["#F7B801", 0.33], ["#A30000"]]}
-                  onComplete={() => {
-                    setTimerRunning(false);
-                  }}
-                >
-                  {renderTime}
-                </CountdownCircleTimer>
-              </div>
-            </div>
-          </section>
+          <Slideshow />
+          <div className={styles.timerWrapper}>
+            <CircularProgressbar
+              value={calculateProgress()}
+              text={formatTime(timeRemaining)}
+              strokeWidth={10}
+            />
+          </div>
+        </Section>
+
+        <Section>
           <div className={styles.timerControls}>
             <ButtonGroup>
-              <button onClick={startTimer}>Start</button>
-              <button onClick={stopTimer}>Stop</button>
-              <button onClick={resetTimer}>Reset</button>
-              <button onClick={() => setShowCards(true)}>Show Cards</button> {/* Button to show the cards */}
+              <Button onClick={startTimer}>Start</Button>
+              <Button onClick={stopTimer}>Stop</Button>
+              <Button onClick={resetTimer}>Reset</Button>
             </ButtonGroup>
           </div>
         </Section>
-      </section>
-      {renderCardsSection()}
-    </div>
+
+        <Typography>Page: {page}</Typography>
+        <Pagination count={3} page={page} onChange={handleChange} />
+      </div>
+    </Sidebar>
   );
 };
 
