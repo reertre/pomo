8793bdce5b42862fbd8e1e0cff275a8fc8274845
Devraj Changkakati <devraj.changkakati@gmail.com>
2023-06-16 10:17:11 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,47 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { CountdownCircleTimer } from "react-countdown-circle-timer";
-import styles from "./styles.module.css";
-
-const renderTime = ({ remainingTime }) => {
-  if (remainingTime === 0) {
-    return <div className={styles.timer}>Too late...</div>;
-  }
-
-  return (
-    <div className={styles.timer}>
-      <div className={styles.text}>Remaining</div>
-      <div className={styles.value}>{remainingTime}</div>
-      <div className={styles.text}>seconds</div>
-    </div>
-  );
-};
-
-function App() {
-  return (
-    <div className={styles.App}>
-      <h1>
-        CountdownCircleTimer
-        <br />
-        React Component
-      </h1>
-      <div className={styles.timerWrapper}>
-        <CountdownCircleTimer
-          isPlaying
-          duration={7}
-          colors={[["#004777", 0.33], ["#F7B801", 0.33], ["#A30000"]]}
-          onComplete={() => [true, 1000]}
-        >
-          {renderTime}
-        </CountdownCircleTimer>
-      </div>
-      <p className={styles.info}>
-        Change component properties in the code field on the right to try
-        different functionalities.
-      </p>
-    </div>
-  );
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+<CircularProgressbar
+  value={calculateProgress(isBreak ? breakTimeRemaining : focusTimeRemaining)}
+  text={formatTime(isBreak ? breakTimeRemaining : focusTimeRemaining)}
+  styles={buildStyles({
+    rotation: 0.25,
+    strokeLinecap: 'butt',
+    textSize: '16px',
+    pathTransitionDuration: 0.5,
+    pathColor: `rgba(62, 152, 199, ${isBreak ? 1 : 0.5})`,
+    textColor: '#f88',
+    trailColor: '#d6d6d6',
+    backgroundColor: '#3e98c7',
+  })}
+/>
