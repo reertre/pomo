0226d92b6748cd4bfe4d9d765935f99102bc505d
Devraj Changkakati <devraj.changkakati@gmail.com>
2023-06-22 12:13:30 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,14 +1,33 @@
-<CircularProgressbar
-  value={calculateProgress(isBreak ? breakTimeRemaining : focusTimeRemaining)}
-  text={formatTime(isBreak ? breakTimeRemaining : focusTimeRemaining)}
-  styles={buildStyles({
-    rotation: 0.25,
-    strokeLinecap: 'butt',
-    textSize: '16px',
-    pathTransitionDuration: 0.5,
-    pathColor: `rgba(62, 152, 199, ${isBreak ? 1 : 0.5})`,
-    textColor: '#f88',
-    trailColor: '#d6d6d6',
-    backgroundColor: '#3e98c7',
-  })}
-/>
+const calculateProgress = () => {
+  let totalDuration;
+
+  if (timerMode === 'pomo') {
+    totalDuration = 6 * 60; // Total duration for pomodoro in seconds
+  } else if (timerMode === 'short') {
+    totalDuration = 30; // Total duration for short break in seconds
+  } else if (timerMode === 'long') {
+    totalDuration = 60 * 60; // Total duration for long break in seconds
+  } else {
+    totalDuration = 0; // Default duration is 0
+  }
+
+  return (TimeRemaining / totalDuration) * 100;
+};
+
+const handleModeChange = (event) => {
+  setTimerMode(event.target.id);
+
+  switch (event.target.id) {
+    case "pomo":
+      setTimeRemaining(6 * 60); // 6 minutes
+      break;
+    case "short":
+      setTimeRemaining(30); // 30 seconds
+      break;
+    case "long":
+      setTimeRemaining(60 * 60); // 60 minutes
+      break;
+    default:
+      setTimeRemaining(6 * 60); // Default to pomodoro mode
+  }
+};
