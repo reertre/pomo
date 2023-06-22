f0a1dfaaab19e34ecfb352f5cf135651cf55def8
Devraj Changkakati <devraj.changkakati@gmail.com>
2023-06-22 15:33:43 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,77 +1,29 @@
-import React, { useState } from "react";
-import { CountdownCircleTimer } from "react-countdown-circle-timer";
-import "./App.css";
+import React, { useState } from 'react';
+import Pomodoro from './Pomodoro';
+import Settings from './Settings';
 
-const minuteSeconds = 60;
+const App = () => {
+  const [workDuration, setWorkDuration] = useState(25);
+  const [shortBreakDuration, setShortBreakDuration] = useState(10);
+  const [longBreakDuration, setLongBreakDuration] = useState(20);
 
-const timerProps = {
-  isPlaying: true,
-  size: 400,
-  strokeWidth: 30
-};
-
-const renderTime = (time) => {
-  const minutes = Math.floor(time / minuteSeconds);
-  const seconds = Math.floor(time % minuteSeconds);
   return (
-    <div className="time-wrapper">
-      <div className="time">
-        {minutes.toString().padStart(2, "0")}:{seconds.toString().padStart(2, "0")}
-      </div>
+    <div>
+      <Settings
+        workDuration={workDuration}
+        setWorkDuration={setWorkDuration}
+        shortBreakDuration={shortBreakDuration}
+        setShortBreakDuration={setShortBreakDuration}
+        longBreakDuration={longBreakDuration}
+        setLongBreakDuration={setLongBreakDuration}
+      />
+      <Pomodoro
+        workDuration={workDuration}
+        shortBreakDuration={shortBreakDuration}
+        longBreakDuration={longBreakDuration}
+      />
     </div>
   );
 };
 
-export default function App() {
-  const [initialTime, setInitialTime] = useState(25);
-  const [isRunning, setIsRunning] = useState(false);
-  const [remainingTime, setRemainingTime] = useState(initialTime * minuteSeconds);
-
-  const startTimer = () => {
-    setIsRunning(true);
-  };
-
-  const stopTimer = () => {
-    setIsRunning(false);
-  };
-
-  const resetTimer = () => {
-    setIsRunning(false);
-    setRemainingTime(initialTime * minuteSeconds);
-  };
-
-  const handleTimeChange = (event) => {
-    const newTime = parseInt(event.target.value);
-    setInitialTime(newTime);
-    setRemainingTime(newTime * minuteSeconds);
-  };
-
-  return (
-    <div className="App">
-      <div className="timer-container">
-        <div className="timer-wrapper">
-          <CountdownCircleTimer
-            {...timerProps}
-            colors="#0344f9"
-            duration={remainingTime}
-            initialRemainingTime={remainingTime}
-            isPlaying={isRunning}
-            onComplete={() => [false, 0]}
-          >
-            {({ elapsedTime }) => renderTime(remainingTime - elapsedTime)}
-          </CountdownCircleTimer>
-        </div>
-
-        <div className="buttons">
-          <button onClick={startTimer}>Start</button>
-          <button onClick={stopTimer}>Stop</button>
-          <button onClick={resetTimer}>Reset</button>
-        </div>
-      </div>
-
-      <div className="settings">
-        <button onClick={() => alert("Open settings modal")}>Settings</button>
-      </div>
-    </div>
-  );
-}
+export default App;
