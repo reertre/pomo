184b9577f534bb3bc74e3c578101752ec8b99da2
Devraj Changkakati <devraj.changkakati@gmail.com>
2024-12-20 00:34:49 +0530

    Update Pomodoro.js

--- Pomodoro.js
+++ Pomodoro.js
@@ -15,34 +15,25 @@ class HierarchyLevelFeed(Feed):
         if "attributes" not in self._sds_hierarchy_attributes_config:
             raise Exception("Did not find attributes to create hierarchy feed.")
 
-        if not isinstance(hierarchy_data, List):
+        if not isinstance(hierarchy_data, list):
             raise Exception("The data should be of List type.")
 
         headers = self._sds_hierarchy_attributes_config["attributes"]
         headers = sub(r"[\n\t\s]*", "", headers).split(",")
 
         # Group hierarchy data by type to avoid repeated filtering
-        grouped_data = {}
-        for item in hierarchy_data:
-            item_type = item.get("type")
-            if item_type not in grouped_data:
-                grouped_data[item_type] = []
-            grouped_data[item_type].append(item)
+        grouped_data = {header: [] for header in headers}
+        for hierarchy in hierarchy_data:
+            type_key = hierarchy.get("type")
+            if type_key in grouped_data:
+                grouped_data[type_key].append(hierarchy)
 
-        flattened_data = {}
-        hierarchy_levels = ["Level10", "Level9", "Level8", "Level7", "Level6", "SubProduct", "BusinessArea", "ProductArea", "Company", "Group"]
+        hierarchy_data.append(grouped_data)  # Append grouped data to hierarchy data
 
-        for level in hierarchy_levels:
-            level_data = grouped_data.get(level, [])
-            ids = [item.get("id") for item in level_data]
-            names = [item.get("name") for item in level_data]
-            flattened_data[f"{level}id"] = ids
-            flattened_data[f"{level}name"] = names
-
-        return self._create_feed_file(headers, hierarchy_data, feed_name), flattened_data
+        return self._create_feed_file(headers, hierarchy_data, feed_name)
 
     def feed(self, hierarchy_data):
         feed_name = self._feed_name(sds_entity="hierarchy", is_json=False)
-        feed_file_content, flattened_data = self._hierarchy_content(hierarchy_data, feed_name)
+        feed_file_content = self._hierarchy_content(hierarchy_data, feed_name)
         self._save_feed(feed_name, feed_file_content)
-        return feed_name, flattened_data
+        return feed_name
